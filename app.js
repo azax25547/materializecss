d32cd67d1391b181ded54151c81bf06498552c93
azax25547 <momentumangular@gmail.com>
2018-11-03 22:22:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+    // Sidenav Initialisation
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  // Carousel Initialisation
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
